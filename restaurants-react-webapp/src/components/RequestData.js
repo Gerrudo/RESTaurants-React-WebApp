dfca93e66367e459c7c19cbfe80b76a0dec32b8c
Thomas Turner <thomasturner@Thomass-MBP.lan>
2020-10-23 17:50:09 +0100

    Added dataReady ? check to render()

--- restaurants-react-webapp/src/components/RequestData.js
+++ restaurants-react-webapp/src/components/RequestData.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import InfoTab from './InfoTab.js';
-const backendDir = 'https://dev.tomsnetwork.uk:1443';
+//const backendDir = 'https://dev.tomsnetwork.uk:1443';
 
 function locationService(){
   return new Promise (function (resolve) {
@@ -8,42 +8,56 @@ function locationService(){
       navigator.geolocation.getCurrentPosition(buildUrl);
       function buildUrl(position) {
         let queryCoordinates = position.coords.latitude + ',' + position.coords.longitude;
-        let requestUrl = `${backendDir}/go#${queryCoordinates}`;
-        resolve(requestUrl)
+        resolve(queryCoordinates)
       }
     } else { 
       console.error('Geolocation not supported by browser');
     }
   })
 }
-async function onMount(){
-  let requestUrl = await locationService();
-  console.log(requestUrl);
-  fetch(requestUrl)
-  .then(res => res.json())
-  .then((data) => {
-    this.setState({ info: data })
-  })
-  .catch(console.log)
-}
 
 class RequestData extends Component {
-    //We will store api data here
+
   state = {
     info : {},
+    dataReady: false
   }
 
-  componentDidMount() {
-    onMount()
-  }
+  async componentWillMount() {
+    let userCoordinates = await locationService();
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("userCoordinates", userCoordinates);
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: 'follow'
+    };
+
+    fetch("https://dev.tomsnetwork.uk:1443/go", requestOptions)
+    .then(response => response.json())
+    .then(result => 
+      this.setState({ 
+        info: result,
+        dataReady: true
+      }),
+    )
+    .catch(error => console.error(error));
+    }
 
   render() {
     return(
       <div className="container">
-        <InfoTab info={this.state.info} />
+        {
+          this.state.dataReady ? <InfoTab info={this.state.info} /> : <h1> Loading </h1>
+        }
       </div>
     )
   }
-}
+}  
 
 export default RequestData;
--- restaurants-react-webapp/src/components/infoTab.js
+++ restaurants-react-webapp/src/components/infoTab.js
@@ -6,17 +6,17 @@ class InfoTab extends React.Component {
 
     return(
       <div>
-        <h2>{info.name}</h2>
+        <h2>{info.result.name}</h2>
           <h3>Information</h3>
           <ul class="list-group">
             <h4>Address</h4>
-              <li class="list-group-item">{info.address}</li>
+              <li class="list-group-item">{info.result.formatted_address}</li>
                     <h4>Phone Number</h4>
-              <li class="list-group-item">{info.phoneNumber}</li>
+              <li class="list-group-item">{info.result.international_phone_number}</li>
                     <h4>Website</h4>
-              <a href={info.url} class="list-group-item">{info.url}</a>
+              <a href={info.url} class="list-group-item">{info.result.website}</a>
                     <h4>Opening Hours</h4>
-              <ul class="list-group">{info.openingHours}</ul>
+              <ul class="list-group">{info.result.opening_hours.weekday_text}</ul>
           </ul>
       </div>
     )
